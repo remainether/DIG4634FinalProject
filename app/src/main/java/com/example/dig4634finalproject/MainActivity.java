b72c838bd0ffbb5f5681ec8e59122e26ff8e79cb
slipsinholo <71987344+slipsinholo@users.noreply.github.com>
2021-11-17 19:32:09 -0500

    Part two

--- app/src/main/java/com/example/dig4634finalproject/MainActivity.java
+++ app/src/main/java/com/example/dig4634finalproject/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     //hello tatumoo
+    //this is part two
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
