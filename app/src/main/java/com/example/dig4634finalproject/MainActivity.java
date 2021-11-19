f53001e228f0e9eef47d78b225bebd565330ae6e
slipsinholo <71987344+slipsinholo@users.noreply.github.com>
2021-11-18 19:39:54 -0500

    APK Layout for Milestone 1

--- app/src/main/java/com/example/dig4634finalproject/MainActivity.java
+++ app/src/main/java/com/example/dig4634finalproject/MainActivity.java
@@ -2,15 +2,39 @@ package com.example.dig4634finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    //hello tatumoo
-    //this is part two
-    //my b
-    @Override
+
+
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+            Button btn = (Button)findViewById(R.id.playMusicButton);
+            Button btn2 = (Button)findViewById(R.id.favoriteSongsButton);
+
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(MainActivity.this, PlaySongs.class));
+                }
+            });
+            btn2.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(MainActivity.this, LikedSongs.class));
+                }
+            });
+
+
+
+        }
+
+
+
 }
+
---
+++ app/src/main/java/com/example/dig4634finalproject/PlaySongs.java
@@ -0,0 +1,14 @@
+package com.example.dig4634finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlaySongs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_songs);
+    }
+}
