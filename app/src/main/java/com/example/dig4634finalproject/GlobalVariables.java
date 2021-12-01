df82f6c38f39ba13f9a785c0f1d2c2c57237b2cf
Spriley Cherenfant <sprileycherenfant@gmail.com>
2021-12-01 15:59:45 -0500

    Added Temperature and Humidity Sensors and Added GlobalVariables - Spriley

---
+++ app/src/main/java/com/example/dig4634finalproject/GlobalVariables.java
@@ -0,0 +1,4 @@
+package com.example.dig4634finalproject;
+
+public class GlobalVariables {
+}
--- app/src/main/java/com/example/dig4634finalproject/PlaySongs.java
+++ app/src/main/java/com/example/dig4634finalproject/PlaySongs.java
@@ -2,13 +2,55 @@ package com.example.dig4634finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 
-public class PlaySongs extends AppCompatActivity {
+public class PlaySongs extends AppCompatActivity implements SensorEventListener {
+
+    SensorManager sensorManagerTemp;
+    SensorManager sensorManagerHum;
+    private Sensor tempSensor;
+    private Sensor humiditySensor;
+    int myTemp = 0;
+    int myHumidity =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_songs);
+
+        sensorManagerTemp = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        sensorManagerTemp.registerListener(this, sensorManagerTemp.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE), sensorManagerTemp.SENSOR_DELAY_NORMAL );
+
+        sensorManagerHum = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        sensorManagerHum.registerListener(this, sensorManagerHum.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY), sensorManagerHum.SENSOR_DELAY_NORMAL );
+
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor sensor = sensorEvent.sensor;
+
+        if (sensor.getType() == Sensor.TYPE_AMBIENT_TEMPERATURE) {
+            myTemp = (int)sensorEvent.values[0];
+        }
+        else if (sensor.getType() == Sensor.TYPE_RELATIVE_HUMIDITY)
+        {
+            myHumidity = (int)sensorEvent.values[0];
+        }
+
+
+        Log.d("EXAMPLE My Temp is: "  , " TEMP: " + myTemp + " my humidity is: " + myHumidity);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
