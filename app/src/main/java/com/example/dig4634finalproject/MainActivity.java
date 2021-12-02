070eafd46c4c3602acc242cc505a290c08a8ad04
Tatum <80289231+tatumoo@users.noreply.github.com>
2021-12-02 18:46:43 -0500

    raw files (wrong encoding), fade in animation

--- app/src/main/java/com/example/dig4634finalproject/MainActivity.java
+++ app/src/main/java/com/example/dig4634finalproject/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
             Button btn = (Button)findViewById(R.id.playMusicButton);
             Button btn2 = (Button)findViewById(R.id.favoriteSongsButton);
+            ImageView bg= (ImageView)findViewById(R.id.mainbg);
+            Animation myFadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fadein);
+            bg.startAnimation(myFadeInAnimation);
 
             btn.setOnClickListener(new View.OnClickListener() {
                 @Override
