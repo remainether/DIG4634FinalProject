bbe8853ddf4e39ba0024b5a16c1e7983853fa899
remainether <92239142+remainether@users.noreply.github.com>
2021-12-02 19:25:41 -0500

    Added visualizer and functional play pause buttons as well as functional media player

--- app/src/main/java/com/example/dig4634finalproject/PlaySongs.java
+++ app/src/main/java/com/example/dig4634finalproject/PlaySongs.java
@@ -1,19 +1,25 @@
 package com.example.dig4634finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.net.Uri;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.MediaController;
 import android.widget.TextView;
-import android.widget.VideoView;
+
+import com.gauravk.audiovisualizer.visualizer.WaveVisualizer;
+
 
 public class PlaySongs extends AppCompatActivity implements SensorEventListener {
 
@@ -26,35 +32,81 @@ public class PlaySongs extends AppCompatActivity implements SensorEventListener 
     TextView humidityText;
     float myTemp = 0;
     int myHumidity =0;
+    Button playBtn, pauseBtn, stopBtn;
+
+    MediaPlayer mediaPlayer;
+
+    WaveVisualizer waveVisualizer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_songs);
-
-        MediaController mediaController = new MediaController(this);
-        final VideoView videoView = findViewById(R.id.videoView);
-        Uri uri = Uri.parse("https://static.videezy.com/system/resources/previews/000/040/193/original/bg_0004.mp4");
-
-        //mediaController.setAnchorView(videoView);
-        //videoView.setMediaController(mediaController);
-
-        videoView.setVideoURI(uri);
-        videoView.requestFocus();
-        videoView.start();
+        playBtn = findViewById(R.id.playButton);
+        pauseBtn = findViewById(R.id.pauseButton);
+        stopBtn = findViewById(R.id.stopButton);
+        waveVisualizer = findViewById(R.id.blast);
 
         weatherIcon = (ImageView) findViewById(R.id.weatherIcon);
         tempText = findViewById(R.id.tempText);
         humidityText = findViewById(R.id.humidityText);
 
-        sensorManagerTemp = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+            sensorManagerTemp = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
         sensorManagerTemp.registerListener(this, sensorManagerTemp.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE), sensorManagerTemp.SENSOR_DELAY_NORMAL );
 
         sensorManagerHum = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
         sensorManagerHum.registerListener(this, sensorManagerHum.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY), sensorManagerHum.SENSOR_DELAY_NORMAL );
 
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)!= PackageManager.PERMISSION_GRANTED){
+
+            requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO},1);
+
+        }
+
+        playBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                if(mediaPlayer == null){
+                    mediaPlayer = MediaPlayer.create(PlaySongs.this, R.raw.hot);
+                    int audioSessionId = mediaPlayer.getAudioSessionId();
+                    if(audioSessionId != -1){
+                        waveVisualizer.setAudioSessionId(audioSessionId);
+                    }
+
+                }
+                mediaPlayer.start();
+            }
+        });
+
+        pauseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mediaPlayer != null){
+                    mediaPlayer.pause();
+                }
+            }
+        });
+
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mediaPlayer != null){
+                    mediaPlayer.release();
+                    mediaPlayer = null;
+                    waveVisualizer.release();
+                }
+
+            }
+        });
+
+
 
     }
+
+
+
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
@@ -69,39 +121,42 @@ public class PlaySongs extends AppCompatActivity implements SensorEventListener 
             myHumidity = (int)sensorEvent.values[0];
         }
 
-        if(myTemp > 100 && myHumidity > 30)
-            GlobalVariables.heatIndex = 5;
+        if(myTemp > 100 && myHumidity > 30 )
+            weatherIcon.setImageResource(R.drawable.hott);
         else if(myTemp > 80 && myHumidity > 20)
-            GlobalVariables.heatIndex = 4;
+            weatherIcon.setImageResource(R.drawable.warmm);
         else if(myTemp > 65 && myHumidity > 10)
-            GlobalVariables.heatIndex = 3;
+            weatherIcon.setImageResource(R.drawable.chilll);
         else if(myTemp > 50 && myHumidity > 0)
-            GlobalVariables.heatIndex = 2;
+            weatherIcon.setImageResource(R.drawable.coldd);
         else if(myTemp < 50 || myHumidity < 0)
-            GlobalVariables.heatIndex = 1;
+            weatherIcon.setImageResource(R.drawable.freezingg);
         tempText.setText(""+ myTemp + "° F");
         humidityText.setText(""+ myHumidity + "% Humidity");
 
+        /*
         switch(GlobalVariables.heatIndex)
         {
             case 1:
                 weatherIcon.setImageResource(R.drawable.freezingg);
-                break;
+                //break;
             case 2:
                 weatherIcon.setImageResource(R.drawable.coldd);
-                break;
+               // break;
             case 3:
                 weatherIcon.setImageResource(R.drawable.chilll);
-                break;
+                //break;
             case 4:
                 weatherIcon.setImageResource(R.drawable.warmm);
-                break;
+               // break;
             case 5:
                 weatherIcon.setImageResource(R.drawable.hott);
-                break;
+                //break;
             default:
                 break;
         }
+
+         */
         Log.d("EXAMPLE My Temp is: "  , " TEMP: " + myTemp + " my humidity is: " + myHumidity + " HEAT INDEX IS: " + GlobalVariables.heatIndex);
     }
 
@@ -109,4 +164,6 @@ public class PlaySongs extends AppCompatActivity implements SensorEventListener 
     public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
+
+
 }
